0ee94058051c5cb2e8730d428c4022b1a58036ba
laloinsane <calisto.michel@gmail.com>
2020-05-08 06:54:50 -0400

    Limpieza de codigo

--- title-screen/ParallaxBackground.gd
+++ title-screen/ParallaxBackground.gd
@@ -1,15 +1,10 @@
 extends ParallaxBackground
 
-var offsets = 0
+var off_set = 0
 
 func _ready():
 	set_process(true)
-	
+
 func _process(delta):
-	offsets -= 100 * delta
-	set_scroll_offset(Vector2(offsets, 0))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	off_set -= 100 * delta
+	set_scroll_offset(Vector2(off_set, 0))
