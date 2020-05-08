4f28476e34ec60ff10817b61d4dab06563104f12
laloinsane <calisto.michel@gmail.com>
2020-05-08 07:13:18 -0400

    Botones de audio ok

--- nivel1/Nivel1.gd
+++ nivel1/Nivel1.gd
@@ -1,20 +1,5 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#$ColorRect.visible = true
-	#$ColorRect/AnimationPlayer.play("fadein")
 	yield($ColorRect/AnimationPlayer, "animation_finished")
 	$ColorRect.visible = false
-	#get_tree().change_scene("res://nivel1/Nivel1.tscn")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
