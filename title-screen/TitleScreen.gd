7ff9598d4185079acf3de06d0a5672d0d0df3b8a
laloinsane <calisto.michel@gmail.com>
2020-05-08 06:48:01 -0400

    TitleScreen limpia

--- title-screen/TitleScreen.gd
+++ title-screen/TitleScreen.gd
@@ -10,7 +10,13 @@ func _on_TextureButton_pressed():
 	get_tree().change_scene("res://nivel1/Nivel1.tscn")
 
 func _on_TextureButton2_pressed():
+	$ColorRect.visible = true
+	$ColorRect/AnimationPlayer.play("fadein")
+	yield($ColorRect/AnimationPlayer, "animation_finished")
 	get_tree().change_scene("res://nivel2/Nivel2.tscn")
 
 func _on_TextureButton3_pressed():
+	$ColorRect.visible = true
+	$ColorRect/AnimationPlayer.play("fadein")
+	yield($ColorRect/AnimationPlayer, "animation_finished")
 	get_tree().change_scene("res://nivel3/Nivel3.tscn")
