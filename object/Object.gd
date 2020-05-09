9fc0b1a8eeee639ed48c6844487ebe1622833bac
laloinsane <calisto.michel@gmail.com>
2020-05-08 23:03:47 -0400

    Seleccion del objeto

--- object/Object.gd
+++ object/Object.gd
@@ -34,28 +34,9 @@ func set_image(x):
 
 func get_image():
 	return image
-	
-#func _on_Object_input_event(viewport, event, shape_idx):
-#	if event is InputEventMouseButton:
-#		if event.is_pressed():
-#			$Line2D.visible = true
-#			print(id,code)
-#			emit_signal("is_code")
-
 
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			print("si nose")
 			$Line2D.visible = true
-			print(id,code)
 			emit_signal("is_code")
-			#if self.is_in_group("deck"):
-			#	emit_signal("in_deck")
-			#else:
-			#	if self.is_in_group("hand"):
-			#		emit_signal("in_hand")
-			#	else:
-			#		if self.is_in_group("defense"):
-			#			$Line2D.visible = true
-			#			emit_signal("in_attack")
