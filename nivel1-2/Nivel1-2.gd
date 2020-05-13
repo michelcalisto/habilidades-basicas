e95a19b3b68c596cf34340f3bcbc900e4d91bf48
laloinsane <calisto.michel@gmail.com>
2020-05-13 19:43:50 -0400

    Estructura del nivel 1-2 ok

--- nivel1-2/Nivel1-2.gd
+++ nivel1-2/Nivel1-2.gd
@@ -14,3 +14,31 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_ToMenu_pressed():
+	pass # Replace with function body.
+
+
+func _on_Escuchar_pressed():
+	pass # Replace with function body.
+
+
+func _on_Si_pressed():
+	pass # Replace with function body.
+
+
+func _on_No_pressed():
+	pass # Replace with function body.
+
+
+func _on_Continuar_pressed():
+	pass # Replace with function body.
+
+
+func _on_Finalizar_pressed():
+	pass # Replace with function body.
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
