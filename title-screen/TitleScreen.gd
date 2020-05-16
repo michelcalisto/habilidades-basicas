52bf47d9703a69bda2fcb1469ffb2b0e9c71ab6f
laloinsane <calisto.michel@gmail.com>
2020-05-16 14:42:23 -0400

    Inicio Nivel 2-1

--- title-screen/TitleScreen.gd
+++ title-screen/TitleScreen.gd
@@ -16,7 +16,7 @@ func _on_TextureButton2_pressed():
 	$ColorRect.visible = true
 	$ColorRect/AnimationPlayer.play("fadein")
 	yield($ColorRect/AnimationPlayer, "animation_finished")
-	get_tree().change_scene("res://nivel1/Nivel1.tscn")
+	get_tree().change_scene("res://nivel2-1/Nivel2-1.tscn")
 
 func _on_TextureButton3_pressed():
 	$ColorRect.visible = true
