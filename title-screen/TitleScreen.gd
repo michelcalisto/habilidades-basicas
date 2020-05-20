152ccf2c2933a3c1b102819835567dd75a97261d
laloinsane <calisto.michel@gmail.com>
2020-05-20 17:32:37 -0400

    Revision titlescreen

--- title-screen/TitleScreen.gd
+++ title-screen/TitleScreen.gd
@@ -6,21 +6,20 @@ func _ready():
 	yield($ColorRect/AnimationPlayer, "animation_finished")
 	$ColorRect.visible = false
 
-func _on_TextureButtonNivel1_pressed():
+func _on_Nivel1_pressed():
 	$ColorRect.visible = true
 	$ColorRect/AnimationPlayer.play("fadein")
 	yield($ColorRect/AnimationPlayer, "animation_finished")
 	get_tree().change_scene("res://nivel1-1/Nivel1-1.tscn")
 
-func _on_TextureButton2_pressed():
+func _on_Nivel2_pressed():
 	$ColorRect.visible = true
 	$ColorRect/AnimationPlayer.play("fadein")
 	yield($ColorRect/AnimationPlayer, "animation_finished")
 	get_tree().change_scene("res://nivel2-1/Nivel2-1.tscn")
 
-func _on_TextureButton3_pressed():
+func _on_Nivel3_pressed():
 	$ColorRect.visible = true
 	$ColorRect/AnimationPlayer.play("fadein")
 	yield($ColorRect/AnimationPlayer, "animation_finished")
 	get_tree().change_scene("res://nivel3-1/Nivel3-1.tscn")
-
