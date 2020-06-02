89381ecba03e3be217e72e8a0fab660a13672105
laloinsane <calisto.michel@gmail.com>
2020-06-02 12:50:57 -0400

    Clean project

--- scripts/Nivel1.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	# Transition
-	$Transition.fadeOut()
-
-
-func _on_HUD_to_title_screen():
-	pass # Replace with function body.
--- scripts/SPopUp.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var info = ""
-
-func update_info(info):
-	self.info = str(info)
